7b8d40fa01275115c61e98c1c44409ef84996bef
Jithin E M <jithinem4007@gmail.com>
2025-01-28 16:31:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Define the server port
+const PORT = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/`);
+});
